37244addbdb2cc58b825ed64f2a0713dcd4e02e6
wacdev <i@wac.tax>
2023-09-06 05:59:05 +0800

    ♨

---
+++ service.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+name=xxai-api
+
+service_sh=/opt/xxai.art/api/run.sh
+
+chmod +x $service_sh
+
+system_service=/etc/systemd/system/$name.service
+cp ./service $system_service
+
+sed -i 's#EXEC#${service_sh}#' $system_service
+
+systemctl daemon-reload
+
+systemctl enable --now $name
+systemctl restart $name
+
+systemctl status $name --no-pager
+
+journalctl -u $name -n 10 --no-pager --no-hostname
