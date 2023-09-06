0d814ecc5688d3b456534278bbcd0799640b9e06
wacdev <i@wac.tax>
2023-09-06 11:25:41 +0800

    ♨

--- env.sh
+++ env.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-
 env_sh() {
   local nowdir=$(pwd)
-  cd "$(realpath $BASH_SOURCE[0])"/../conf/conn
+  cd "$(dirname $(realpath ${BASH_SOURCE[0]}))"/../conf/conn
   local i
   for i in $@; do
     set -o allexport
